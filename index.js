fbf3e7e8ecfc32b6c6e09e73126e5a0304f71ec6
Awais khan <awais9986755@gmail.com>
2025-09-09 22:10:00 -0700

    first prjoct

---
+++ index.js
@@ -0,0 +1,24 @@
+// require('dotenv').config()
+// This is my first project in backend ...
+import express from 'express'
+const PORT = 3000
+
+const app = express();
+
+app.get('/',function (req,res){
+    res.send("<h1>app write documentation na rashid jor shawe de...</h1>")
+})
+
+app.get('/login',function (req,res){
+    res.send("<h1>This is a login page</h1>")
+})
+
+app.get('/signin',function (req,res){
+    res.send("<h1>This is a sign in page... ok </h1>")
+})
+
+app.listen(PORT,()=>{
+    console.log(`This is a backend project ${PORT}`)
+})
+
+
